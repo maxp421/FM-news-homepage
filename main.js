9fc4bc54bf96f9d229fcaf77324085056d097e07
Maxp421 <penzes.max@gmail.com>
2023-02-01 14:20:24 +0100

    added overlay, fixed accessibility issues

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const bodyOverlay = document.querySelector("body");
 
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
@@ -7,9 +8,11 @@ navToggle.addEventListener("click", () => {
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
+    bodyOverlay.setAttribute("data-overlay", true);
   }
   else {
     primaryNav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
+    bodyOverlay.setAttribute("data-overlay", false);
   }
 })
